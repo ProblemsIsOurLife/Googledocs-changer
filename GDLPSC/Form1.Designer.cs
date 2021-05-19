71a030b8d7057bcecf7eca1bf49c5fb4dcb7dc60
Ralions <vikbok00@gmail.com>
2021-05-19 14:37:47 +0500

    Добавьте файлы проекта.

---
+++ GDLPSC/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GDLPSC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.connect = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(389, 12);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(151, 44);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "Подключить/обновить таблицу";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(409, 91);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(115, 44);
+            this.save.TabIndex = 1;
+            this.save.Text = "Сохранить изменения";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(352, 205);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(370, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 224);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.connect);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GSheetsApp";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GDLPSC/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using System.Threading;
+
+
+namespace GDLPSC
+{
+   
+    public partial class Form1 : Form
+    {
+        bool inwork = false;
+        private static string[,] data = new string[10, 3];
+        private static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        private static readonly string AppName = "DBLPC";
+        //Id дока
+        private static readonly string SpreadsheetId = "1la6IP9FReLgd_Xf-QIdMaeie7_LCvuEjAPMKYocpdSE";
+        private String Range = "A1:C10";
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            inwork = true;
+
+            dataGridView1.MultiSelect = false;
+
+            dataGridView1.RowCount = 10;
+            dataGridView1.ColumnCount = 3;
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 3; j++)
+                    data[i, j]="";
+            var credential = GetSheetCredentionals();
+           var service = GetService(credential);
+           GetCells(service, Range, SpreadsheetId);
+            for(int i = 0; i < 10; i++)  
+                  for(int j=0; j<3;j++)
+                 dataGridView1.Rows[i].Cells[j].Value = data[i,j];
+            for (int j = 0; j < 3; j++) {
+                dataGridView1.Columns[j].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            pictureBox1.Visible = Visible;
+
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (inwork == true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        data[i, j] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+
+                }
+                string message2 = "Данные успешно сохранены";
+                string caption2 = "Успех";
+                MessageBoxButtons buttons2 = MessageBoxButtons.OK;
+                DialogResult result2;
+                inwork = true;
+                result2 = MessageBox.Show(message2, caption2, buttons2);
+               
+               }
+            else
+            {
+                string message = "Нет данных. Загрузить?";
+                string caption = "Error";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+                inwork = true;
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    dataGridView1.RowCount = 10;
+                    dataGridView1.ColumnCount = 3;
+                    var credential1 = GetSheetCredentionals();
+                    var service1 = GetService(credential1);
+                    GetCells(service1, Range, SpreadsheetId);
+                    for (int i = 0; i < 10; i++)
+                        for (int j = 0; j < 3; j++)
+                            dataGridView1.Rows[i].Cells[j].Value = data[i, j];
+                    for (int j = 0; j < 3; j++)
+                    {
+                        dataGridView1.Columns[j].SortMode = DataGridViewColumnSortMode.NotSortable;
+                    }
+                }
+                pictureBox1.Visible = Visible;
+            }
+
+
+
+
+
+            var credential = GetSheetCredentionals();
+            var service = GetService(credential);
+            FillSpreadsheet(service, SpreadsheetId, data);
+        }
+
+
+
+
+        private static UserCredential GetSheetCredentionals()
+        {  
+            //файл авторизации
+            using (var stream =
+               new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                return GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+
+            }
+        }
+        private static SheetsService GetService(UserCredential credential)
+        {
+            return new SheetsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = AppName
+            });
+        }
+        private static void FillSpreadsheet(SheetsService service, string SpreadsheetId, string[,] data)
+        {
+            List<Request> requests = new List<Request>();
+            for (int i=0; i < data.GetLength(0); i++)
+            {
+                List<CellData> values = new List<CellData>();
+                for (int j = 0;j < data.GetLength(1); j++)
+                {
+                    values.Add(new CellData
+                    {
+                        UserEnteredValue = new ExtendedValue
+                        {
+                            StringValue = data[i, j]
+                        }
+                    });
+                }
+                requests.Add(
+                    new Request
+                    {
+                        UpdateCells = new UpdateCellsRequest
+                        {
+                            Start = new GridCoordinate
+                            {
+                                SheetId = 0,
+                                RowIndex = i,
+                                ColumnIndex = 0
+                            },
+                            Rows = new List<RowData> { new RowData { Values = values } },
+                            Fields = "userEnteredValue"
+                        }
+                    }
+                    ); 
+            }
+            BatchUpdateSpreadsheetRequest busr = new BatchUpdateSpreadsheetRequest
+            {
+                Requests = requests
+            };
+            service.Spreadsheets.BatchUpdate(busr, SpreadsheetId).Execute();
+        }
+        
+        private static void GetCells(SheetsService service, string range, string spreadsheetId)
+        {
+            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+            ValueRange response = request.Execute();
+            
+            int i = 0;
+            foreach (var value in response.Values)
+            {
+                data[i, 0] += value[0];
+                data[i, 1] += value[1];
+                data[i, 2] += value[2];
+                i++;
+            }
+            
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+                DataGridViewCell cell = dataGridView1.CurrentCell;
+                dataGridView1.CurrentCell = cell;
+                dataGridView1.BeginEdit(true);         
+        }
+    }
+    
+}
